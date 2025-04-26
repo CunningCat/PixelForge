6b571e87a215bb0a0d3eafbd539e269780670aaa
rabbit <cunning_rabbit@163.com>
2025-04-26 16:49:19 +0800

    登出逻辑完成

--- src/components/user/DropdownMenu.tsx
+++ src/components/user/DropdownMenu.tsx
@@ -3,7 +3,10 @@ export default function DropdownMenu({isOpen}: {isOpen: boolean}) {
     <div className="w-24 py-2 bg-slate-800 rounded-lg absolute right-0 z-10 mt-40" style={{display: isOpen ? 'block' : 'none'}}>
       <a href="#" className="block w-full text-center py-2 hover:text-orange-200">个人信息</a>
       <hr className="w-full border-slate-600 " />
-      <a href="#" className="block w-full text-center py-2 hover:text-orange-200">登出</a>
+      <a className="block w-full text-center py-2 hover:text-orange-200"
+      onClick={() => {localStorage.removeItem('userId')
+        window.location.reload()}
+      }>登出</a>
     </div>
   );
 }
