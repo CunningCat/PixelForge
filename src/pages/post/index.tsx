a4d84962b1464a02422b5d61fbfb045faa60778a
rabbit <cunning_rabbit@163.com>
2025-05-16 18:26:23 +0800

    点击更多继续加载帖子

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,25 +1,30 @@
 import PostItem from "./PostItem"
 import Header from "../home/Header"
-import {  useState } from "react";
+import {  useRef, useState } from "react";
 import  {PostDownloadInfo}  from "@/types/postdownloadinfo";
-import { toast } from "sonner";
 import { getLatestNews } from "@/services/getLatestNews";
 import { useEffect } from "react";
+
 export default function Post() {
+  const [hasMore, setHasMore] = useState(true);
+  const [postList, setPostList] = useState<PostDownloadInfo[]>([]);
+  const offset =useRef(0);
   
-  const [postList, setPostList] = useState<PostDownloadInfo[] | null>([]);
+
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await getLatestNews();
-      if (res) {
-        
-        setPostList(res);
-      } else {
-        toast("失败", { description: "获取帖子失败。请稍后尝试" });
-      }
-    };
-    fetchData();
+    loadPosts();
   }, []);
+  const loadPosts = async () => {
+    
+    const res = await getLatestNews(offset.current);
+    if (res && res.length > 0) {
+      setPostList(prev => [...prev, ...res]);
+      offset.current += res.length;
+       if (res.length < 5) {
+        setHasMore(false); // 已经没有更多了
+    }
+    }
+  }
 
   return (
     <div className="flex flex-col bg-gray-50 min-h-screen">
@@ -27,7 +32,7 @@ export default function Post() {
       <main className="flex flex-col mt-10 gap-10 w-[50%] mx-auto ">
         {postList?postList.map((item) => <PostItem className="bg-gray-100" key={item.post_id} item={item}/>)
         : <div>加载中...</div>}
-        
+        <button onClick={loadPosts}>{hasMore ? "加载更多" : "没有更多内容了"}</button>
       </main>
       
     </div>
